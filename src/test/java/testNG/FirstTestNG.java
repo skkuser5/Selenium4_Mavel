b343ca1165fff50d9864336bfbc3f148f0447c35
srinivas <srinivas@testgmail.com>
2020-09-10 15:47:35 +0100

    fixed the scrpts

--- src/test/java/testNG/FirstTestNG.java
+++ src/test/java/testNG/FirstTestNG.java
@@ -5,11 +5,11 @@ import org.testng.annotations.Test;
 public class FirstTestNG {
   @Test
   public void testCase1() {
-	  System.out.println("in testcase1");
+	  System.out.println("in testcase1 making changes");
   }
   
   @Test(enabled = false)
   public void testCase2() {
-	  System.out.println("in testcase2");
+	  System.out.println("in testcase2 making changes");
   }
 }
